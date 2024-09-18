19323ff2c916e4294386add1271cd01a41b218b3
Lukas <git@lukasnotes.dk>
2024-09-18 13:21:44 +0200

    add more comments

--- examples/with-unix-socket/index.ts
+++ examples/with-unix-socket/index.ts
@@ -5,7 +5,7 @@ import { sql } from 'drizzle-orm';
 // Docs
 // https://orm.drizzle.team/docs/get-started-postgresql#neon-postgres
 // Test with
-// NEON_WS_PROXY_HOST=$(docker compose port neon 80) bun run serverless.ts
+// NEON_WS_PROXY_HOST=$(docker compose port neon 80) bun run index.ts
 
 const NEON_WS_PROXY_HOST = process.env.NEON_WS_PROXY_HOST;
 console.log('NEON_WS_PROXY_HOST', NEON_WS_PROXY_HOST);
@@ -18,12 +18,14 @@ if (!NEON_WS_PROXY_HOST) {
 neonConfig.wsProxy = () => `${process.env.NEON_WS_PROXY_HOST}/v1`;
 
 // Disable TLS when running on local machine
-neonConfig.useSecureWebSocket = false;
+neonConfig.useSecureWebSocket = false; // or true, if you, for example, expose using Cloudflare Tunnel
 // Disable all authentication and encryption
 neonConfig.pipelineTLS = false;
 neonConfig.pipelineConnect = false;
 
 const connectionString = `pgsql://postgres:neon-password@placeholder/neon-db`;
+// "placeholder" can be any string just to satisfy Neon's connection string format,
+// because we're using Unix socket, we don't need to specify the host and port at all.
 
 const pool = new Pool({ connectionString });
 const db = drizzle(pool);
